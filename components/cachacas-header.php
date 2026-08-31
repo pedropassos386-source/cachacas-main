a26f4d230987ebcb4f5e989c3277c389ebfcb791
Pedro Passos <pedropassos386@gmail.com>
2026-08-31 10:58:01 -0300

    refactor: usa header compartilhado nas páginas de cachaças

--- components/cachacas-header.php
+++ components/cachacas-header.php
@@ -1,13 +1,4 @@
-<a class="skip-link" href="#conteudo-principal">Pular para o conteúdo</a>
-<header class="header">
-    <a href="index.html" aria-label="Cachaças de Novo Cruzeiro — página inicial">
-        <img src="assets/img/logo.png" alt="Cachaças Novo Cruzeiro" width="500" height="500" decoding="async">
-    </a>
-
-    <nav>
-        <ul class="menu">
-            <li><a href="index.html">Home</a></li>
-            <li><a href="pages/premiation/premiation.php">Premiações</a></li>
-        </ul>
-    </nav>
-</header>
+<?php
+$basePath = '.';
+$currentPage = $currentPage ?? '';
+require __DIR__ . '/site-header.php';
