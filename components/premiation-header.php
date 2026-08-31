90b1a5d7d035ffe5b288b8d257426003b10e312e
Pedro Passos <pedropassos386@gmail.com>
2026-08-31 10:58:08 -0300

    refactor: usa header compartilhado em premiações

--- components/premiation-header.php
+++ components/premiation-header.php
@@ -1,11 +1,4 @@
- <a class="skip-link" href="#conteudo-principal">Pular para o conteúdo</a>
- <header id="home">
-     <a href="../../index.html" aria-label="Cachaças de Novo Cruzeiro — página inicial"><img src="../../assets/img/logo.png" alt="Cachaças Novo Cruzeiro" width="500" height="500"></a>
-
-     <nav>
-         <ul class="menu">
-
-             <li><a href="../../index.html">Home</a></li>
-         </ul>
-     </nav>
- </header>
+<?php
+$basePath = '../..';
+$currentPage = 'premiation';
+require __DIR__ . '/site-header.php';
