02a806e3f04683c7232cefb0d1b00a7d48ceda63
Pedro Passos <pedropassos386@gmail.com>
2026-08-31 11:12:26 -0300

    refactor: add normalized page and image paths

---
+++ mineirinha.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cachaça Mineirinha | Tradição regional de Minas Gerais</title>
+    <meta name="description" content="Conheça a história e a produção da Cachaça Ribeirinha, tradicional na região de Novo Cruzeiro, Minas Gerais.">
+    <meta name="robots" content="index, follow">
+    <link rel="stylesheet" href="assets/css/cachaca-reset.css">
+    <link rel="stylesheet" href="assets/css/cachaca.css">
+    <link rel="stylesheet" href="assets/css/responsivo.css">
+</head>
+
+<body>
+    <?php require './components/cachacas-header.php'; ?>
+
+    <main id="conteudo-principal">
+        <h1 class="product-title">Cachaça Mineirinha</h1>
+        <div class="container">
+            <div class="mini-imagens">
+                <div class="mini-imagem">
+                    <img src="assets/img/Photos-1-001/Mineirinha.jpg" alt="Imagem Mineirinha" id="front">
+                </div>
+
+                <div class="mini-imagem">
+                    <img src="assets/img/Photos-1-001/Mineirinha2.jpg" alt="Imagem Mineirinha" id="side">
+                </div>
+
+                <div class="cachacas-social">
+                    <span class="instagram" aria-hidden="true">
+                        <img class="instagram-icon" src="assets/icons/instagram.svg" alt="Icone instagram">
+                    </span>
+                </div>
+            </div>
+
+            <div class="imagem-principal">
+                <img src="assets/img/Photos-1-001/Mineirinha.jpg" alt="imagem principal Mineirinha" id="img">
+            </div>
+
+            <section class="description">
+                <h2>Sobre</h2>
+
+                <ul class="description-list">
+                    <li>Cachaça artesanal produzida e engarrafada por João Ferreira dos Santos</li>
+
+                    <li><span>Local de Fabricação:</span>Fazenda Mandassaia</li>
+
+                    <li><span>Desde quando:</span>Sem data</li>
+                </ul>
+            </section>
+        </div>
+    </main>
+
+
+    <script src="assets/js/app.js" defer></script>
+    <script src="assets/js/zoom.js" defer></script>
+</body>
+
+</html>
